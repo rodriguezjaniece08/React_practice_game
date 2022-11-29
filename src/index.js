34b9512886f4a5603d5211a55b84428fcbb99c27
Janice R <rodriguezjaniece@gmail.com>
2022-11-29 17:01:42 -0500

    changed the square class to be a function component and set x as default in board constructor

--- src/index.js
+++ src/index.js
@@ -3,25 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 
-class Square extends React.Component {
-    render() {
-      return (
-        <button 
-            className="square" 
-            onClick={()=>
-            this.props.onClick()}
-        >
-            {this.props.value}
-        </button>
-      );
-    }
-  }
+function square(props) {
+    return (
+      <button className="square" onClick= 
+      {props.onClick}>
+        {props.value}
+      </button>
+     );
+   }
+     
+     
+
+    
+  
   
   class Board extends React.Component {
     constructor(props){
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
@@ -30,7 +31,7 @@ class Square extends React.Component {
         squares[i] = 'X';
         this.setState({squares: squares});
     }
-    
+
     renderSquare(i) {
       return (
         <Square 
